e1a980b150e06b649ba937f6faf6f7823a1674c7
Maria Rosa Scoleri <scoleri.mariarosa@gmail.com>
2024-05-20 17:09:49 +0200

    defined linear projection and encoder

--- autoencoder.py
+++ autoencoder.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, SAGEConv, GATv2Conv, GINConv, PNAConv
+from torch_geometric.nn import GCNConv, SAGEConv, GATv2Conv, GINConv, PNAConv, global_add_pool, global_mean_pool, global_max_pool
 from torch_geometric.nn import global_add_pool
 
 class LinearProjection(nn.Module):
@@ -29,7 +29,7 @@ class LinearProjection(nn.Module):
     def forward(self, batch):
         batch_indices = batch.batch
         batch_unique = torch.unique(batch_indices)
-        new_batch = []
+        new_x = []
         for el in batch_unique:
             select = batch.x[batch_indices==el]
             verb = select[0]
@@ -37,52 +37,16 @@ class LinearProjection(nn.Module):
 
             # handle the verb: take all the features
             temp = self.dropout(self.relu(self.l_norm(self.verb_fc1(verb))))
-            new_batch.append(self.l_norm2(self.verb_fc2(temp)))
+            new_x.append(self.l_norm2(self.verb_fc2(temp)))
 
             # handle the objects: take x[:object_dim]
             for obj in objs:
                 temp = self.dropout(self.relu(self.l_norm(self.obj_fc1(obj[:self.obj_dim]))))
-                new_batch.append(self.l_norm2(self.obj_fc2(temp)))
+                new_x.append(self.l_norm2(self.obj_fc2(temp)))
 
-        new_batch = torch.stack(new_batch, dim=0)
-        return new_batch
-
-class GIN(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
-        super().__init__()
-        self.dropout = dropout
-        
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(GINConv(nn.Sequential(nn.Linear(input_dim, hidden_dim),  
-                            nn.LeakyReLU(0.2),
-                            nn.BatchNorm1d(hidden_dim),
-                            nn.Linear(hidden_dim, hidden_dim), 
-                            nn.LeakyReLU(0.2))
-                            ))                        
-        for layer in range(n_layers-1):
-            self.convs.append(GINConv(nn.Sequential(nn.Linear(hidden_dim, hidden_dim),  
-                            nn.LeakyReLU(0.2),
-                            nn.BatchNorm1d(hidden_dim),
-                            nn.Linear(hidden_dim, hidden_dim), 
-                            nn.LeakyReLU(0.2))
-                            )) 
-
-        self.bn = nn.BatchNorm1d(hidden_dim)
-        self.fc = nn.Linear(hidden_dim, latent_dim)
-        
-
-    def forward(self, data):
-        edge_index = data.edge_index
-        x = data.x
-
-        for conv in self.convs:
-            x = conv(x, edge_index)
-            x = F.dropout(x, self.dropout, training=self.training)
-
-        out = global_add_pool(x, data.batch)
-        out = self.bn(out)
-        out = self.fc(out)
-        return out
+        new_x = torch.stack(new_x, dim=0)
+        batch.x = new_x
+        return batch
     
 class myGNN(nn.Module):
     ''' 
@@ -90,12 +54,12 @@ class myGNN(nn.Module):
         simply returning the elementwise max or mean between the two nodes that 
         form the edge    
     '''
-    def __init__(self, layer_type, input_dim, hidden_dim, output_dim, dropout_prob, edge_creation, device):
+    def __init__(self, layer_type, input_dim, hidden_dim, output_dim, pool_type, dropout_prob, device):
         super().__init__()
         self.output_dim = output_dim
         self.device = device
-        self.edge_creation = edge_creation
         self.layer_type = layer_type
+        self.pool_type = pool_type
 
         if layer_type=='gcn':
             self.conv1 = GCNConv(input_dim, hidden_dim)
@@ -107,18 +71,33 @@ class myGNN(nn.Module):
             self.conv1 = GATv2Conv(input_dim, hidden_dim)
             self.conv2 = GATv2Conv(hidden_dim, output_dim)
         elif layer_type=='gin':
-            self.conv1 = GINConv(input_dim, hidden_dim)
-            self.conv2 = GINConv(hidden_dim, output_dim)
+            self.conv1 = GINConv(nn.Sequential(
+                nn.Linear(input_dim, hidden_dim),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(hidden_dim)
+            ))
+            self.conv2 = GINConv(nn.Sequential(
+                nn.Linear(hidden_dim, output_dim),
+                nn.LeakyReLU(0.2),
+            ))
         else:
             raise Exception('Wrong graph layer type')
+        
         self.dropout = nn.Dropout(dropout_prob)
         self.relu = nn.ReLU()
-        self.adaptive_max = nn.AdaptiveMaxPool1d(output_dim)
 
-    def forward(self, nodes_features, edge_index):
-        nodes_features = self.dropout(self.relu(self.conv1(nodes_features, edge_index)))
-        nodes_features = self.dropout(self.relu(self.conv2(nodes_features, edge_index)))
-        return nodes_features
+    def forward(self, batch):
+        x = self.dropout(self.relu(self.conv1(batch.x, batch.edge_index)))
+        x = self.dropout(self.relu(self.conv2(x, batch.edge_index)))
+        if self.pool_type == 'add':
+            x = global_add_pool(x, batch.batch)
+        elif self.pool_type == 'mean':
+            x = global_mean_pool(x, batch.batch)
+        elif self.pool_type == 'max':
+            x = global_max_pool(x, batch.batch)
+        else:
+            raise Exception('Wrong pooling type')
+        return x
     
 # Decoder
 class Decoder(nn.Module):
@@ -147,25 +126,32 @@ class Decoder(nn.Module):
         adj[:,idx[0],idx[1]] = x
         adj = adj + torch.transpose(adj, 1, 2)
         return adj
-
-# Autoencoder
-class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim_enc, hidden_dim_dec, latent_dim, n_layers_enc, n_layers_dec, n_max_nodes):
-        super(AutoEncoder, self).__init__()
+    
+class MyAutoEncoder(nn.Module):
+    def __init__(self, object_feats_dim, verb_feats_dim, 
+                 num_rels, num_verbs, num_objs, 
+                 hidden_projection_dim, projection_dim, hidden_dim, latent_dim,
+                 hidden_dim_dec, n_layers_dec,
+                 n_max_nodes, pool_type='add', device = 'cuda', dropout_prob=0.2, graph_type='gat'):
+        super().__init__()
+        self.projection_dim = projection_dim
+        self.graph_type = graph_type
         self.n_max_nodes = n_max_nodes
-        self.input_dim = input_dim
-        self.encoder = GIN(input_dim, hidden_dim_enc, latent_dim, n_layers_enc)
+
+        self.linear_projection = LinearProjection(verb_feats_dim, object_feats_dim, hidden_projection_dim, projection_dim, device, dropout_prob)
+        self.encoder = myGNN(graph_type, projection_dim, hidden_dim, latent_dim, pool_type, dropout_prob, device)
         self.decoder = Decoder(latent_dim, hidden_dim_dec, n_layers_dec, n_max_nodes)
 
-    def forward(self, data):
-        x_g = self.encoder(data)
+    def forward(self, batch):
+        lp = self.linear_projection(batch)
+        x_g = self.encoder(lp)
         adj = self.decoder(x_g)
         return adj
-
+    
     def encode(self, data):
         x_g = self.encoder(data)
         return x_g
-
+    
     def decode(self, x_g):
         adj = self.decoder(x_g)
         return adj
