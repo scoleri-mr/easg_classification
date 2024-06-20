3eb6d6ac547d54a812c0f4f71c2dcfaec8028da3
Maria Rosa Scoleri <scoleri.mariarosa@gmail.com>
2024-06-20 15:00:25 +0200

    minor mod

--- autoencoder.py
+++ autoencoder.py
@@ -278,25 +278,6 @@ class EASGvae(nn.Module):
        adj = self.decoder(x_g)
        return adj
 
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2, reduction='mean'):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction='none')
-        pt = torch.exp(-ce_loss)
-        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
-
-        if self.reduction == 'mean':
-            return focal_loss.mean()
-        elif self.reduction == 'sum':
-            return focal_loss.sum()
-        else:
-            return focal_loss
-
 class MultiClassFocalLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2, reduction='mean'):
         super(MultiClassFocalLoss, self).__init__()
