b4336f92a412ea95209bf5d2b0884a0e86fc430d
Maria Rosa Scoleri <scoleri.mariarosa@gmail.com>
2024-09-11 00:01:21 +0200

    random window for validation

--- dataset_video/dataset_video.py
+++ dataset_video/dataset_video.py
@@ -17,21 +17,33 @@ def filter_short_videos(dataset_original, threshold):
             video_ids.append(video_id)
     return long_videos, video_ids
 
+def filter_short_triplets(triplets, threshold):
+    long_triplets = []
+    video_ids = []
+    for video_id, triplet in triplets.items():
+        if len(triplet)>=threshold:
+            long_triplets.append(triplet)
+            video_ids.append(video_id)
+    return long_triplets, video_ids
+
 def random_window_selection(video, window_size):
     num_frames = video.size(0)
     if num_frames == window_size:
         random_start = 0    #if I have exactly the number of frames as the window_size return the whole video, random.randint doesn't work
     else: 
         random_start = random.randint(0, num_frames - window_size - 1)
-    return video[random_start:random_start+window_size]
+    return video[random_start:random_start+window_size], random_start
 
-def get_subvideos(self, train_video_original):
-    final_videos = []
-    for video in train_video_original:
-        k = (int((len(video)-self.window)/self.shift))+1   # number of subvideos that can be extracted from the current video
+def get_subvideos(long_videos, long_triplets, window, shift):
+    subvideos = []
+    subtriplets = []
+    for triplet, video in zip(long_triplets, long_videos):
+        k = (int((len(video)-window)/shift))+1   # number of subvideos that can be extracted from the current video
         for i in range(k):
-            final_videos.append(video[i*self.shift : i*self.shift+self.window])
-    return final_videos
+            subvideos.append(video[i*shift : i*shift+window])
+            subtriplets.append(triplet[i*shift : i*shift+window])
+    return subvideos, subtriplets
+
 
 class EASGvideo(Dataset):
     def __init__(self, dataset_path, triplets_path, threshold=20, window_size=20, original=False, train_mode=True):
@@ -43,6 +55,8 @@ class EASGvideo(Dataset):
         self.dataset_original = torch.load(dataset_path)
         self.all_triplets = torch.load(triplets_path)
         self.long_videos, self.long_video_ids = filter_short_videos(self.dataset_original, self.threshold)
+        self.long_triplets, self.long_video_ids_triplets = filter_short_triplets(self.all_triplets, self.window_size)
+        assert self.long_video_ids == self.long_video_ids_triplets  # check that the ids are the same
 
         if self.window_size > self.threshold:
             raise Exception("window_size > threshold, may try to get more frames than available.")
@@ -52,14 +66,12 @@ class EASGvideo(Dataset):
 
     def __getitem__(self, idx):
         if self.train_mode:
-            return random_window_selection(self.long_videos[idx], self.window_size)
+            random_window, _ =random_window_selection(self.long_videos[idx], self.window_size)
+            return random_window
         else:
-            return self.long_videos[idx][:self.window_size, :], self.get_triplets(idx)[:self.window_size]
-    
-    def get_triplets(self, idx): 
-        video_id = self.long_video_ids[idx]
-        triplets = self.all_triplets[video_id]
-        return triplets
+            random_window, random_start =random_window_selection(self.long_videos[idx], self.window_size)
+            return random_window, self.long_triplets[idx][random_start:random_start+self.window_size]    
+
     
 def main():
     dataset = EASGvideo("easg_classification/dataset_video/encoded_videos_train_256.pth", threshold=20, window_size=20, original=False)
