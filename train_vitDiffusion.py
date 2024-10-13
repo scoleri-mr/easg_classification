fb6fbcb8e0519e39a22e949dc0df736672529439
Maria Rosa Scoleri <scoleri.mariarosa@gmail.com>
2024-10-13 03:47:28 +0200

    minor mod

--- train_vitDiffusion.py
+++ train_vitDiffusion.py
@@ -40,7 +40,7 @@ def parse_args():
     parser.add_argument('--hidden_dim_denoise', type=int, default=256)
     parser.add_argument('--no_train_denoiser', action='store_false', dest='train_denoiser', help="If specified, do not train the denoiser.")
     parser.add_argument('--no_wandb', action='store_false', dest='wandb', help="If specified disables wandb logging")
-    parser.add_argument('--wandb_proj', type=str, default='ViTDiffusion_newLoss')
+    parser.add_argument('--wandb_proj', type=str, default='ViTDiffusion_final')
     parser.add_argument('--evaluation', action='store_true', help='Evaluation mode')
     parser.add_argument('--diffusion_path', type=str, help='path to the trained diffusion model')
     parser.add_argument('--scheduler_type', type=str, help="Choose 'step' for StepLR and 'warmup' for CosineAnnealingWarmupRestarts. Use lr as parameter for max_lr in warmup.", default='warmup')
@@ -160,7 +160,7 @@ def main():
                 if args.wandb: wandb.log({"val loss": val_loss_all/val_count})
 
                 # checkpoint
-                save_dir = f"./experiments/ViTDiffusion_newLoss/{args.exp_name}/checkpoints"
+                save_dir = f"./experiments/ViTDiffusion_final/{args.exp_name}/checkpoints"
                 os.makedirs(save_dir, exist_ok=True)
                 save_checkpoint(model=denoise_model, optimizer=optimizer, epoch=epoch, path=osp.join(save_dir, "last.ckpt"))
             
